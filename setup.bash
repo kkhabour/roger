e0ef3d13602a2a5677e93f71246dc482316826b0
kkhabour <khabourikarimm@gmail.com>
2019-10-24 23:41:17 +0000

    rogeer

--- setup.bash
+++ setup.bash
@@ -24,6 +24,7 @@ systemctl restart systemd-networkd
 
 
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.origine
+rm -f /etc/ssh/sshd_config
 
 echo "
 # Roger configuration
@@ -40,5 +41,57 @@ PrintMotd no
 AcceptEnv LANG LC_*
 Subsystem sftp  /usr/lib/openssh/sftp-server
 " >> /etc/ssh/sshd_config
+service ssh restart
+
+
+
+
+# Disabling ufw
+ufw --force disable
+
+# Resetting ufw
+ufw --force reset
+
+# Deny All Incoming Connections
+ufw default deny incoming
+
+# Allow All Outgoing Connections
+ufw default allow outgoing
+
+# Allow SSH connection/Limit
+ufw limit 5050/tcp
+
+# HTTP
+ufw allow 80/tcp
+
+# HTTPS
+ufw allow 443/tcp
+
+# Enabling ufw
+ufw --force enable
+
+
+
+apt-get install -y fail2ban
+systemctl start fail2ban
+systemctl enable fail2ban
+
+
+echo "
+[DEFAULT]
+
+[sshd]
+enabled = false
+
+[ssh]
+enabled  = true
+port     = 5050
+filter   = sshd
+logpath  = /var/log/auth.log
+maxretry = 4
+findtime = 20
+bantime = 1m
+" > /etc/fail2ban/jail.local
+service fail2ban restart
 
 
