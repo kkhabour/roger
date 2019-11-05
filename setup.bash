03e854aa14c4cca7968a39bc2c8f61f895803f2b
karim khabouri <46485026+kkhabour@users.noreply.github.com>
2019-11-05 22:49:37 +0100

    roger2

--- setup.bash
+++ setup.bash
@@ -4,6 +4,7 @@
 STATIC_IP=$(hostname -I | awk '{print $1}')/16
 SSH_PORT=5050
 
+echo "$STATIC_IP"
 # update and upgrade packages
 
 apt update -y
@@ -12,13 +13,14 @@ apt upgrade -y
 
 # set static ip
 
+echo "setting static ip ..."
 rm -f /etc/netplan/*
 echo "
 network:
     ethernets:
         enp0s3:
             dhcp4: false
-            addresses: [$STATICIP]
+            addresses: [ $STATIC_IP ]
             gateway4: 10.12.254.254
             nameservers:
                 addresses: [10.23.1.253]
@@ -27,24 +29,30 @@ network:
 
 netplan apply
 systemctl restart systemd-networkd
+echo "static ip done"
 
 # set firewall rules
+
+echo "setting firewall rules ..."
 
 ufw --force disable
 ufw --force reset
 ufw default deny incoming
 ufw default allow outgoing
-ufw limit $(SSH_PORT)/tcp
+ufw limit $SSH_PORT/tcp
 ufw allow 80/tcp
 ufw allow 443/tcp
 ufw --force enable
 
+echo "firewall rules done."
 
 # set SSH configuration
+echo "set ssh configuration ..."
+
 mv /etc/ssh/sshd_config /etc/ssh/sshd_config.origine
 echo "
 # Roger configuration
-Port $(SSH_PORT)
+Port $SSH_PORT
 PasswordAuthentication no
 PubKeyAuthentication yes
 PermitRootLogin no
@@ -59,14 +67,17 @@ Subsystem sftp  /usr/lib/openssh/sftp-server
 " > /etc/ssh/sshd_config
 service ssh restart
 
+echo "ssh config done"
+
 # config fail2ban
+echo "setting fail2ban and apache2 ..."
 apt install fail2ban -y
 apt install apache2 -y
 
 echo "
 [sshd]
 enabled  = true
-port     = $(SSH_PORT)
+port     = $SSH_PORT
 filter   = sshd
 logpath  = /var/log/auth.log
 maxretry = 3
@@ -86,24 +97,26 @@ filter   = portscan
 logpath  = /var/log/ufw.log
 action   = ufw
 maxretry = 5
-ignoreip = 10.12.254.254"
+ignoreip = 10.12.254.254" > /etc/fail2ban/jail.local
 
 echo "
 [Definition]
 failregex = ^<HOST> -.*\"(GET|POST).*
-ignoreregex =" > http-get-dos.conf
+ignoreregex =" > /etc/fail2ban/filter.d/http-get-dos.conf
 
 echo "
 [Definition]
 failregex = UFW BLOCK.* SRC=<HOST>
-ignoreregex =" > portscan.conf
+ignoreregex =" > /etc/fail2ban/filter.d/portscan.conf
 
 fail2ban-client restart
 service fail2ban restart
 
 
+echo "fail2ban configuration done"
 
 # setup crontab jobs
+echo "set cron ..."
 
 echo "
 #!/bin/bash
@@ -116,23 +129,26 @@ apt upgrade -y >> /var/log/update_script.log
 echo "
 #!/bin/bash
 
-OLD=$(cat /etc/cronsum)
-NEW=$(md5sum /etc/crontab | awk '{print $1}')
+OLD=\$(cat /etc/cronsum)
+NEW=\$(md5sum /etc/crontab | awk '{print \$1}')
 
 [ ! -e /etc/crontab ] && exit
 
 if [ ! -e /etc/cronsum ]
 then
-	echo $NEW > /etc/cronsum
+	echo \$NEW > /etc/cronsum
 	exit
 fi
 
-if [ $OLD != $NEW ]
+if [ \$OLD != \$NEW ]
 then
-	mail -s 'Crontab file has been changed' root@roger <<< 'crontab file has changed.'
-	echo $NEW > /etc/cronsum
+	mail -s \"Crontab file has been changed\" root@roger <<< \"crontab file has changed.\"
+	echo \$NEW > /etc/cronsum
 fi" > /etc/cron.d/checker.bash
 
+crontab -r
 crontab -l | { cat; echo -e "@reboot bash /etc/cron.d/update.bash"; } | crontab -
 crontab -l | { cat; echo -e "0 4 * * 1 bash /etc/cron.d/update.bash"; } | crontab -
 crontab -l | { cat; echo -e "0 0 * * * bash /etc/cron.d/checker.bash"; } | crontab -
+
+echo "cron done"
